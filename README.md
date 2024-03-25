15be8fcfa66ffa097cbdb6a89ac8a0d734e91e9b
haleyk10198 <haleyk100198@gmail.com>
2024-03-25 16:18:46 +0800

    #18: initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # modern-cpp-euler
+
+Project Euler done with high level, expressive, yet highly-performance C++-20 code. 
+
+Competitive Programming done clean.
---
+++ problem_18.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <charconv>
+#include <chrono>
+#include <generator>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <ranges>
+#include <string>
+#include <string_view>
+#include <thread>
+#include <type_traits>
+#include <vector>
+
+using std::literals::string_view_literals::operator""sv;
+using boost::multiprecision::cpp_int;
+using namespace std::chrono;
+
+std::generator<std::chrono::year_month_weekday> first_sundays() {
+    for (auto mm = 1901y / January; mm < 2001y / January; mm += months(1)) {
+        co_yield mm / Sunday[1];
+    }
+}
+
+int main() {
+    auto ans = first_sundays() | std::views::filter([](auto&& candidate) {
+        year_month_day ymd{sys_days{candidate}};
+
+        return ymd.day() == 1d;
+    }) |
+               std::ranges::to<std::vector>();
+
+    std::cout << ans.size() << '\n';
+    return 0;
+}
